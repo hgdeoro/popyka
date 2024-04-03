c9eb29dbbc8e15493c7cf8ef61c35eac1511266c
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-03 22:55:49 +0000

    Implement test reading from stream of messages

--- tests/test_pg_connect.py
+++ tests/test_pg_connect.py
@@ -1,8 +1,9 @@
 import logging
 import threading
+import time
 import uuid
 
-import pytest
+import psycopg2.extras
 from psycopg2.extensions import connection as Connection
 from psycopg2.extras import ReplicationCursor
 
@@ -26,7 +27,7 @@ def _db_activity_simulator(
     cn: Connection,
     table_name: str,
     repl_starting_soon_event: threading.Event,
-    db_activity_simulator_done: threading.Event,
+    done: threading.Event,
 ):
     with cn.cursor() as cur:
         cur: ReplicationCursor
@@ -36,8 +37,7 @@ def _db_activity_simulator(
         cn.commit()
 
         logger.info("Table created, waiting for event to start inserting data...")
-        event_received = repl_starting_soon_event.wait(timeout=3)
-        assert event_received is True
+        assert repl_starting_soon_event.wait(timeout=3) is True
 
         cur.execute(f"INSERT INTO {table_name} (NAME) VALUES (gen_random_uuid())")
         cur.execute(f"INSERT INTO {table_name} (NAME) VALUES (gen_random_uuid())")
@@ -46,10 +46,35 @@ def _db_activity_simulator(
         cur.execute(f"INSERT INTO {table_name} (NAME) VALUES (gen_random_uuid())")
         cn.commit()
 
-    db_activity_simulator_done.set()
+    done.set()
 
 
-def test__db_activity_simulator(conn: Connection, conn2: Connection):
+def _db_stream_consumer(cn: Connection, repl_starting_soon_event: threading.Event, payloads: list, max_payloads=15):
+    with cn.cursor() as cur:
+        cur.create_replication_slot("pytest_logical", output_plugin="test_decoding")
+        cur.start_replication(slot_name="pytest_logical", decode=True)
+
+        class DemoConsumer(object):
+            def __call__(self, msg: psycopg2.extras.ReplicationMessage):
+                logger.info("DemoConsumer received payload: %s", msg.payload)
+                payloads.append(msg.payload)
+                msg.cursor.send_feedback(flush_lsn=msg.data_start)
+
+                if len(payloads) == max_payloads:
+                    raise psycopg2.extras.StopReplication()
+
+        consumer = DemoConsumer()
+
+        repl_starting_soon_event.set()
+        try:
+            cur.consume_stream(consumer)
+        except psycopg2.extras.StopReplication:
+            pass
+
+        # TODO: close stream?
+
+
+def test_db_activity_simulator(conn: Connection, conn2: Connection):
     repl_starting_soon_event = threading.Event()
     db_activity_simulator_done = threading.Event()
 
@@ -70,30 +95,35 @@ def test__db_activity_simulator(conn: Connection, conn2: Connection):
         assert cur.fetchall() == [(5,)]
 
 
-@pytest.mark.skip
-def test_start_replication_2(conn: Connection, conn2: Connection, drop_slot):
+def test_start_replication(conn: Connection, conn2: Connection, drop_slot):
+    table_name = f"TEST_TABLE_{uuid.uuid4().hex}"
+    payloads: list = []
     repl_starting_soon_event = threading.Event()
     db_activity_simulator_done = threading.Event()
 
     db_activity_simulator = threading.Thread(
         target=_db_activity_simulator,
         daemon=True,
-        args=[conn, repl_starting_soon_event, db_activity_simulator_done],
+        args=[conn, table_name, repl_starting_soon_event, db_activity_simulator_done],
     )
-    db_activity_simulator.start()
-    db_activity_simulator.join()
 
+    db_stream_consumer = threading.Thread(
+        target=_db_stream_consumer,
+        daemon=True,
+        args=[conn2, repl_starting_soon_event, payloads],
+    )
+
+    db_activity_simulator.start()
+    db_stream_consumer.start()
+
+    db_activity_simulator.join()
     assert db_activity_simulator_done.is_set()
 
-    # with conn.cursor() as cur:
-    #     cur.create_replication_slot('pytest_logical', output_plugin='test_decoding')
-    #     cur.start_replication(slot_name='pytest_logical', decode=True)
-    #
-    #     class DemoConsumer(object):
-    #         def __call__(self, msg):
-    #             print(msg.payload)
-    #             msg.cursor.send_feedback(flush_lsn=msg.data_start)
-    #
-    #     consumer = DemoConsumer()
-    #
-    #     cur.consume_stream(consumer)
+    while len(payloads) < 15:
+        logger.info("There are %s items in 'payloads'", len(payloads))
+        time.sleep(0.2)
+
+    db_stream_consumer.join()
+
+    payloads = [_ for _ in payloads if "INSERT:" in _]
+    assert len(payloads) == 5
