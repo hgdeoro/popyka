c7f0920babbc8cbd8a534c78ba4dd305ad6cbde1
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-03 19:27:56 +0000

    Add test to start replication

--- tests/test_pg_connect.py
+++ tests/test_pg_connect.py
@@ -1,9 +1,23 @@
 import psycopg2
+import pytest
+from psycopg2.extensions import connection as Connection
+import psycopg2.extras
 
 
-def test_connect_to_template1():
-    conn = psycopg2.connect("host=localhost port=5432 dbname=template1 user=postgres")
-    cur = conn.cursor()
-    cur.execute("SELECT 1")
-    records = cur.fetchall()
-    assert records == [(1, )]
+@pytest.fixture
+def conn():
+    dsn = "host=localhost port=5432 dbname=template1 user=postgres"
+    with psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection) as cn:
+        yield cn
+
+
+def test_connect_to_template1(conn: Connection):
+    with conn.cursor() as cur:
+        cur.execute("SELECT 1")
+        records = cur.fetchall()
+        assert records == [(1, )]
+
+
+def test_start_replication(conn: Connection):
+    with conn.cursor() as cur:
+        cur.start_replication(slot_name='pytest', decode=True)
