780d09b1415953ae21a20ff40dc9258f1d654124
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-03 20:09:39 +0000

    Modify fixture to avoid use of tx

--- tests/test_pg_connect.py
+++ tests/test_pg_connect.py
@@ -12,14 +12,20 @@ logger = logging.getLogger(__name__)
 @pytest.fixture
 def conn():
     dsn = "host=localhost port=5432 dbname=template1 user=postgres"
-    with psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection) as cn:
-        with cn.cursor() as cur:
-            cur.execute("SELECT slot_name, slot_type, active FROM pg_replication_slots")
-            results = cur.fetchall()
-            for slot_name, slot_type, active in results:
-                logger.warning("Dropping replication slot %s", slot_name)
-                cur.execute("SELECT pg_drop_replication_slot(%s)", [slot_name])
-        yield cn
+    cn: Connection = psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection)
+    with cn.cursor() as cur:
+        cur.execute("SELECT slot_name, slot_type, active FROM pg_replication_slots")
+        results = cur.fetchall()
+        for slot_name, slot_type, active in results:
+            logger.warning("Dropping replication slot %s", slot_name)
+            cur.execute("SELECT pg_drop_replication_slot(%s)", [slot_name])
+
+    yield cn
+
+    try:
+        cn.close()
+    except:
+        logger.exception("Exception detected when trying to close connection")
 
 
 def test_connect_to_template1(conn: Connection):
@@ -35,10 +41,8 @@ def test_start_without_replication_slots(conn: Connection):
         assert cur.fetchall() == []
 
 
-def test_start_replication_2():
-    dsn = "host=localhost port=5432 dbname=template1 user=postgres"
-    cn = psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection)
-    cur = cn.cursor()
+def test_start_replication_2(conn: Connection):
+    cur = conn.cursor()
     try:
         # test_decoding produces textual output
         cur.start_replication(slot_name='pytest_logical', decode=True)
