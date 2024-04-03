150aab9d58673709ec8dd1aee869b52bb3acd347
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-03 20:04:52 +0000

    Make logical replication work

--- tests/test_pg_connect.py
+++ tests/test_pg_connect.py
@@ -1,13 +1,24 @@
+import logging
+
 import psycopg2
 import pytest
 from psycopg2.extensions import connection as Connection
 import psycopg2.extras
 
 
+logger = logging.getLogger(__name__)
+
+
 @pytest.fixture
 def conn():
     dsn = "host=localhost port=5432 dbname=template1 user=postgres"
     with psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection) as cn:
+        with cn.cursor() as cur:
+            cur.execute("SELECT slot_name, slot_type, active FROM pg_replication_slots")
+            results = cur.fetchall()
+            for slot_name, slot_type, active in results:
+                logger.warning("Dropping replication slot %s", slot_name)
+                cur.execute("SELECT pg_drop_replication_slot(%s)", [slot_name])
         yield cn
 
 
@@ -18,6 +29,26 @@ def test_connect_to_template1(conn: Connection):
         assert records == [(1, )]
 
 
-def test_start_replication(conn: Connection):
+def test_start_without_replication_slots(conn: Connection):
     with conn.cursor() as cur:
-        cur.start_replication(slot_name='pytest', decode=True)
+        cur.execute("SELECT slot_name, slot_type, active FROM pg_replication_slots")
+        assert cur.fetchall() == []
+
+
+def test_pg_create_physical_replication_slot(conn: Connection):
+    with conn.cursor() as cur:
+        cur: psycopg2.extras.ReplicationCursor
+        cur.execute("SELECT * FROM pg_create_physical_replication_slot('pytest')")
+        assert cur.fetchone()
+
+
+def test_start_replication_2():
+    dsn = "host=localhost port=5432 dbname=template1 user=postgres"
+    cn = psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection)
+    cur = cn.cursor()
+    try:
+        # test_decoding produces textual output
+        cur.start_replication(slot_name='pytest_logical', decode=True)
+    except psycopg2.ProgrammingError:
+        cur.create_replication_slot('pytest_logical', output_plugin='test_decoding')
+        cur.start_replication(slot_name='pytest_logical', decode=True)
