7a49dbec52406a20f5fc4b03820f804412c848c4
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-04 20:08:08 +0000

    Refactor function to DbStreamConsumer

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -12,53 +12,64 @@ from tests.test_db_activity_simulator import DbActivitySimulator
 logger = logging.getLogger(__name__)
 
 
-def _db_stream_consumer(cn: Connection, db_activity_simulator: DbActivitySimulator, payloads: list, max_payloads: int):
-    with cn.cursor() as cur:
-        cur.create_replication_slot("pytest_logical", output_plugin="wal2json")
-        cur.start_replication(slot_name="pytest_logical", decode=True)
+class DbStreamConsumer(threading.Thread):
+    def __init__(self, cn: Connection, db_activity_simulator: DbActivitySimulator, max_payloads: int):
+        super().__init__(daemon=True)
+        self._cn = cn
+        self._db_activity_simulator = db_activity_simulator
+        self._max_payloads = max_payloads
+        self._payloads = []
 
-        class DemoConsumer(object):
-            def __call__(self, msg: psycopg2.extras.ReplicationMessage):
-                logger.info("DemoConsumer received payload: %s", msg.payload)
-                payloads.append(msg.payload)
-                msg.cursor.send_feedback(flush_lsn=msg.data_start)
+    @property
+    def payloads(self) -> list:
+        return self._payloads
 
-                if len(payloads) == max_payloads:
-                    raise psycopg2.extras.StopReplication()
+    @property
+    def payloads_parsed(self) -> list[dict]:
+        return [json.loads(_) for _ in self._payloads]
 
-        consumer = DemoConsumer()
+    def run(self) -> None:
+        with self._cn.cursor() as cur:
+            cur.create_replication_slot("pytest_logical", output_plugin="wal2json")
+            cur.start_replication(slot_name="pytest_logical", decode=True)
 
-        db_activity_simulator.start_activity()
-        try:
-            cur.consume_stream(consumer)
-        except psycopg2.extras.StopReplication:
-            pass
+            _payloads = self._payloads
+            _max_payloads = self._max_payloads
 
-        # TODO: close stream?
+            class DemoConsumer(object):
+                def __call__(self, msg: psycopg2.extras.ReplicationMessage):
+                    logger.info("DemoConsumer received payload: %s", msg.payload)
+                    _payloads.append(msg.payload)
+                    msg.cursor.send_feedback(flush_lsn=msg.data_start)
+
+                    if len(_payloads) == _max_payloads:
+                        raise psycopg2.extras.StopReplication()
+
+            consumer = DemoConsumer()
+
+            self._db_activity_simulator.start_activity()
+            try:
+                cur.consume_stream(consumer)
+            except psycopg2.extras.StopReplication:
+                pass
+
+            # TODO: close stream?
 
 
 def test_insert_are_replicated(conn: Connection, conn2: Connection, drop_slot, table_name: str):
     uuids = [str(uuid.uuid4()) for _ in range(4)]
     statements = [("INSERT INTO {table_name} (NAME) VALUES (%s)", [_]) for _ in uuids]
 
-    payloads: list = []
-
     db_activity_simulator = DbActivitySimulator(conn, table_name, statements)
-
-    db_stream_consumer = threading.Thread(
-        target=_db_stream_consumer,
-        daemon=True,
-        args=[conn2, db_activity_simulator, payloads, len(statements)],
-    )
+    db_stream_consumer = DbStreamConsumer(conn2, db_activity_simulator, len(statements))
 
     db_activity_simulator.start()
     db_stream_consumer.start()
-
     db_activity_simulator.join()
     assert db_activity_simulator.is_done
 
-    while len(payloads) < len(statements):
-        logger.info("There are %s items in 'payloads'", len(payloads))
+    while len(db_stream_consumer.payloads) < len(statements):
+        logger.info("There are %s items in 'payloads'", len(db_stream_consumer.payloads))
         time.sleep(0.2)
 
     db_stream_consumer.join()
@@ -82,6 +93,6 @@ def test_insert_are_replicated(conn: Connection, conn2: Connection, drop_slot, t
     #     ]
     # },
 
-    payloads = [json.loads(_) for _ in payloads]
-    assert [_["change"][0]["kind"] for _ in payloads] == ["insert"] * 4
-    assert [_["change"][0]["columnvalues"][0] for _ in payloads] == uuids
+    json_payloads = db_stream_consumer.payloads_parsed
+    assert [_["change"][0]["kind"] for _ in json_payloads] == ["insert"] * 4
+    assert [_["change"][0]["columnvalues"][0] for _ in json_payloads] == uuids
