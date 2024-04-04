ee271f10982efb7e69525c5946398707630d6ec7
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-04 19:53:43 +0000

    Remove no longer code and test

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -6,36 +6,10 @@ import uuid
 
 import psycopg2.extras
 from psycopg2.extensions import connection as Connection
-from psycopg2.extras import ReplicationCursor
 
 from tests.test_db_activity_simulator import DbActivitySimulator
 
 logger = logging.getLogger(__name__)
-
-
-def _db_activity_simulator(
-    cn: Connection,
-    table_name: str,
-    repl_starting_soon_event: threading.Event,
-    done: threading.Event,
-    statements: tuple[tuple[str, tuple]],
-):
-    with cn.cursor() as cur:
-        cur: ReplicationCursor
-        cur.execute(f"DROP TABLE IF EXISTS {table_name}")
-        cn.commit()
-        cur.execute(f"CREATE TABLE {table_name} (NAME VARCHAR)")
-        cn.commit()
-
-        logger.info("Table %s created, waiting for event to start inserting data...", table_name)
-        assert repl_starting_soon_event.wait(timeout=3) is True
-
-        for stmt in statements:
-            logger.info("%s | %s", table_name, str(stmt))
-            cur.execute(stmt[0], stmt[1])
-            cn.commit()
-
-    done.set()
 
 
 def _db_stream_consumer(cn: Connection, db_activity_simulator: DbActivitySimulator, payloads: list, max_payloads: int):
@@ -61,32 +35,6 @@ def _db_stream_consumer(cn: Connection, db_activity_simulator: DbActivitySimulat
             pass
 
         # TODO: close stream?
-
-
-def test_db_activity_simulator_fn(conn: Connection, conn2: Connection):
-    repl_starting_soon_event = threading.Event()
-    db_activity_simulator_done = threading.Event()
-
-    table_name = f"TEST_TABLE_{uuid.uuid4().hex}"
-    statements = (
-        (f"INSERT INTO {table_name} (NAME) VALUES (gen_random_uuid())", []),
-        (f"INSERT INTO {table_name} (NAME) VALUES (gen_random_uuid())", []),
-        (f"INSERT INTO {table_name} (NAME) VALUES (gen_random_uuid())", []),
-    )
-    db_activity_simulator = threading.Thread(
-        target=_db_activity_simulator,
-        daemon=True,
-        args=[conn, table_name, repl_starting_soon_event, db_activity_simulator_done, statements],
-    )
-    db_activity_simulator.start()
-    repl_starting_soon_event.set()
-    db_activity_simulator.join()
-
-    assert db_activity_simulator_done.is_set()
-
-    with conn2.cursor() as cur:
-        cur.execute(f"SELECT count(*) FROM {table_name}")
-        assert cur.fetchall() == [(3,)]
 
 
 def test_insert_are_replicated(conn: Connection, conn2: Connection, drop_slot, table_name: str):
