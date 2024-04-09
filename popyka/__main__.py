9776a35fbf37bfed4a15f09a1ceb96ef1bd95bd1
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-09 19:14:48 +0000

    Refactor: move Server to core

--- popyka/__main__.py
+++ popyka/__main__.py
@@ -1,63 +1,14 @@
-import abc
 import logging
 from urllib.parse import urlparse
 
 import psycopg2.extras
 from psycopg2.extensions import connection as Connection
-from psycopg2.extras import ReplicationCursor
 
-from popyka.core import (
-    POPYKA_DB_DSN,
-    Filter,
-    Processor,
-    ReplicationConsumerToProcessorAdaptor,
-)
+from popyka.core import POPYKA_DB_DSN, Filter, Processor, Server
 from popyka.filters import IgnoreTxFilter
 from popyka.processors import LogChangeProcessor, ProduceToKafkaProcessor
 
 logger = logging.getLogger(__name__)
-
-
-class Server(abc.ABC):
-    @abc.abstractmethod
-    def get_filters(self) -> list[Filter]:
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_processors(self) -> list[Processor]:
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_slot_name(self) -> str:
-        # TODO: let user overwrite via env variables
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_connection(self) -> Connection:
-        """Returns a psycopg2 connection"""
-        # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_dsn(self) -> tuple[str, object, str]:
-        """Return DSN, parsed URI and database name"""
-        raise NotImplementedError()
-
-    def run(self):
-        adaptor = ReplicationConsumerToProcessorAdaptor(self.get_processors(), self.get_filters())
-        cn = self.get_connection()
-        slot_name = self.get_slot_name()
-
-        with cn.cursor() as cur:
-            cur: ReplicationCursor
-            try:
-                cur.create_replication_slot(slot_name, output_plugin="wal2json")
-                logger.info("Replication slot %s created", slot_name)
-            except psycopg2.errors.DuplicateObject:
-                logger.info("Replication slot %s already exists", slot_name)
-
-            cur.start_replication(slot_name=slot_name, decode=True, options={"format-version": "2"})
-            cur.consume_stream(adaptor)
 
 
 class Main(Server):
