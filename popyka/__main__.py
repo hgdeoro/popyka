f74a3ea0b34e6ff63602b4d4eb4ae8d51286f585
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-09 18:51:51 +0000

    Refactor Main into parent class Server

--- popyka/__main__.py
+++ popyka/__main__.py
@@ -1,3 +1,4 @@
+import abc
 import logging
 from urllib.parse import urlparse
 
@@ -17,33 +18,32 @@ from popyka.processors import LogChangeProcessor, ProduceToKafkaProcessor
 logger = logging.getLogger(__name__)
 
 
-class Main:
+class Server(abc.ABC):
+    @abc.abstractmethod
     def get_filters(self) -> list[Filter]:
-        # TODO: take class name of filters from env
-        return [IgnoreTxFilter()]
+        raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_processors(self) -> list[Processor]:
-        # TODO: take class name of processors from env
-        return [LogChangeProcessor(), ProduceToKafkaProcessor()]
+        raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_slot_name(self) -> str:
         # TODO: let user overwrite via env variables
-        _, _, db = self.get_dsn()
-        return f"popyka_{db}"
+        raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_connection(self) -> Connection:
+        """Returns a psycopg2 connection"""
         # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-        dsn, parsed, db = self.get_dsn()
-        logger.info("DSN host=%s port=%s user=%s db=%s", parsed.hostname, parsed.port, parsed.username, db)
-        return psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection)
+        raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_dsn(self) -> tuple[str, object, str]:
-        """Return DSN, also parsed URI and database name"""
-        parsed = urlparse(POPYKA_DB_DSN)
-        assert parsed.path.startswith("/")
-        return POPYKA_DB_DSN, parsed, parsed.path[1:]
+        """Return DSN, parsed URI and database name"""
+        raise NotImplementedError()
 
-    def main(self):
+    def run(self):
         adaptor = ReplicationConsumerToProcessorAdaptor(self.get_processors(), self.get_filters())
         cn = self.get_connection()
         slot_name = self.get_slot_name()
@@ -60,6 +60,29 @@ class Main:
             cur.consume_stream(adaptor)
 
 
+class Main(Server):
+    def get_filters(self) -> list[Filter]:
+        return [IgnoreTxFilter()]
+
+    def get_processors(self) -> list[Processor]:
+        return [LogChangeProcessor(), ProduceToKafkaProcessor()]
+
+    def get_slot_name(self) -> str:
+        _, _, db = self.get_dsn()
+        return f"popyka_{db}"
+
+    def get_connection(self) -> Connection:
+        # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+        dsn, parsed, db = self.get_dsn()
+        logger.info("DSN host=%s port=%s user=%s db=%s", parsed.hostname, parsed.port, parsed.username, db)
+        return psycopg2.connect(dsn, connection_factory=psycopg2.extras.LogicalReplicationConnection)
+
+    def get_dsn(self) -> tuple[str, object, str]:
+        parsed = urlparse(POPYKA_DB_DSN)
+        assert parsed.path.startswith("/")
+        return POPYKA_DB_DSN, parsed, parsed.path[1:]
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    Main().main()
+    Main().run()
