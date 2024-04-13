962a87fcc067c4462bf60225ddf51cab836417af
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-13 10:50:21 +0000

    Implement interpolation of env variables

--- popyka/config.py
+++ popyka/config.py
@@ -1,3 +1,4 @@
+import copy
 import dataclasses
 import importlib
 import pathlib
@@ -108,11 +109,12 @@ class PopykaConfig:
     processors: list[ProcessorConfig]
 
     @classmethod
-    def from_yaml(cls, config: dict) -> "PopykaConfig":
+    def from_yaml(cls, config: dict, environment: dict[str, str] = None) -> "PopykaConfig":
         # FIXME: a better name would be 'from_dict()'
-        database_config = DatabaseConfig.from_yaml(config["database"])
-        filters = [FilterConfig.from_yaml(_) for _ in config["filters"]]
-        processors = [ProcessorConfig.from_yaml(_) for _ in config["processors"]]
+        interpolated = Interpolator(environment=environment or {}).interpolate(config)
+        database_config = DatabaseConfig.from_yaml(interpolated["database"])
+        filters = [FilterConfig.from_yaml(_) for _ in interpolated["filters"]]
+        processors = [ProcessorConfig.from_yaml(_) for _ in interpolated["processors"]]
         return PopykaConfig(
             database=database_config,
             filters=filters,
@@ -120,7 +122,50 @@ class PopykaConfig:
         )
 
     @classmethod
-    def get_default_config(cls) -> "PopykaConfig":
+    def get_default_config(cls, environment=None) -> "PopykaConfig":
         config_path = pathlib.Path(__file__).parent / "popyka-default.yaml"
         config_dict = yaml.safe_load(config_path.read_text())
-        return PopykaConfig.from_yaml(config_dict)
+        return PopykaConfig.from_yaml(config_dict, environment=environment)
+
+
+class Interpolator:
+    def __init__(self, environment: dict[str, str]):
+        self._environment = environment
+
+    def _interpolate_list(self, element: list) -> list:
+        assert isinstance(element, list)
+        for i in range(len(element)):
+            element[i] = self._interpolate(element[i])
+        return element
+
+    def _interpolate_dict(self, element: dict) -> dict:
+        assert isinstance(element, dict)
+        for key in element.keys():
+            value = element[key]
+            new_value = self._interpolate(value)
+            element[key] = new_value
+        return element
+
+    def _interpolate_str(self, element: str) -> str:
+        assert isinstance(element, str)
+        for env_key, env_value in self._environment.items():
+            element = element.replace("${" + env_key + "}", env_value)
+        return element
+
+    def _interpolate(self, element: [list, dict, str, bool, int]) -> list | dict | str | bool | int:
+        if isinstance(element, list):
+            return self._interpolate_list(element)
+        elif isinstance(element, dict):
+            return self._interpolate_dict(element)
+        elif isinstance(element, str):
+            return self._interpolate_str(element)
+        elif isinstance(element, (bool, int)):
+            return element
+        else:
+            raise NotImplementedError(f"Cannot do interpolation on element of type {type(element)}: {element}")
+
+    def interpolate(self, config: dict) -> dict:
+        assert isinstance(config, dict)
+        interpolated_config = copy.deepcopy(config)
+        interpolated_config = self._interpolate(interpolated_config)
+        return interpolated_config
---
+++ tests/test_interpolator.py
@@ -0,0 +1,97 @@
+import json
+import uuid
+
+from popyka.config import Interpolator
+
+# FIXME: test with all yaml data types (int, bool, etc.)
+
+
+def test_interpolator_without_env():
+    original_config = {
+        "key_1": "value_1",
+        "key_2": "value_2",
+        "dict_plain": {
+            "key_1": "value_1",
+            "key_2": "value_2",
+        },
+        "dict_complex": {
+            "key_1": "value_1",
+            "list_str": ["value_1", "value_2"],
+            "list_dict_str": [
+                {
+                    "k1": "v1",
+                    "k2": "v2",
+                },
+                {
+                    "k3": "v3",
+                    "k4": "v4",
+                },
+            ],
+        },
+    }
+    interpolator = Interpolator(environment={})
+    config = interpolator.interpolate(config=original_config)
+    assert config is not original_config
+    assert config == original_config
+
+
+def test_interpolator_with_env():
+    environment: dict[str, str] = {
+        "INTERPOLATION_A": str(uuid.uuid4()),
+        "INTERPOLATION_B": str(uuid.uuid4()),
+        "INTERPOLATION_C": str(uuid.uuid4()),
+        "INTERPOLATION_D": str(uuid.uuid4()),
+        "INTERPOLATION_E": str(uuid.uuid4()),
+    }
+
+    original_config = {
+        "key_1": "${INTERPOLATION_A}",
+        "key_2": "value_2",
+        "dict_plain": {
+            "key_1": "${INTERPOLATION_B}",
+            "key_2": "value_2",
+        },
+        "dict_complex": {
+            "key_1": "${INTERPOLATION_C}",
+            "list_str": ["value_1", "${INTERPOLATION_D}"],
+            "list_dict_str": [
+                {
+                    "k1": "${INTERPOLATION_E}",
+                    "k2": "v2",
+                },
+                {
+                    "k3": "v3",
+                    "k4": "v4",
+                },
+            ],
+        },
+    }
+
+    expected_config = {
+        "key_1": environment["INTERPOLATION_A"],
+        "key_2": "value_2",
+        "dict_plain": {
+            "key_1": environment["INTERPOLATION_B"],
+            "key_2": "value_2",
+        },
+        "dict_complex": {
+            "key_1": environment["INTERPOLATION_C"],
+            "list_str": ["value_1", environment["INTERPOLATION_D"]],
+            "list_dict_str": [
+                {
+                    "k1": environment["INTERPOLATION_E"],
+                    "k2": "v2",
+                },
+                {
+                    "k3": "v3",
+                    "k4": "v4",
+                },
+            ],
+        },
+    }
+
+    interpolator = Interpolator(environment=environment)
+    config = interpolator.interpolate(config=original_config)
+    print(json.dumps(config, indent=4))
+    assert config is not original_config
+    assert config == expected_config
