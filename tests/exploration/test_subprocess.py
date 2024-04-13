32d31e0a74bcf7ac1c58847b7c94120ff8309ea9
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-13 20:38:20 +0000

    Move SubProcCollector to module

--- tests/exploration/test_subprocess.py
+++ tests/exploration/test_subprocess.py
@@ -1,48 +1,10 @@
-import io
-import logging
-import subprocess
-import threading
 import time
 
-logger = logging.getLogger(__name__)
-
-
-def _read_into_list(buffered_reader: io.BufferedReader, target_list: list[str]):
-    while True:
-        target_list.append(buffered_reader.readline())
-
-
-class SubProcess:
-    def __init__(self, args):
-        self._args = args
-        self._proc = None
-        self._stdout: list[str] = []
-        self._stderr: list[str] = []
-        self._thread_stdout = None
-        self._thread_stderr = None
-
-    @property
-    def stdout(self):
-        return self._stdout
-
-    @property
-    def stderr(self):
-        return self._stderr
-
-    def start(self):
-        self._proc = subprocess.Popen(args=self._args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        self._thread_stdout = threading.Thread(
-            target=_read_into_list, args=[self._proc.stdout, self._stdout], daemon=True
-        )
-        self._thread_stderr = threading.Thread(
-            target=_read_into_list, args=[self._proc.stderr, self._stderr], daemon=True
-        )
-        self._thread_stdout.start()
-        self._thread_stderr.start()
+from tests.subp_collector import SubProcCollector
 
 
 def test():
-    sp = SubProcess(args=["vmstat", "1"])
+    sp = SubProcCollector(args=["vmstat", "1", "3"])
     sp.start()
     while True:
         print(f"STDOUT: {len(sp.stdout)}")
---
+++ tests/subp_collector.py
@@ -0,0 +1,40 @@
+import io
+import logging
+import subprocess
+import threading
+
+logger = logging.getLogger(__name__)
+
+
+def _read_into_list(buffered_reader: io.BufferedReader, target_list: list[str]):
+    while True:
+        target_list.append(buffered_reader.readline())
+
+
+class SubProcCollector:
+    def __init__(self, args):
+        self._args = args
+        self._proc = None
+        self._stdout: list[str] = []
+        self._stderr: list[str] = []
+        self._thread_stdout = None
+        self._thread_stderr = None
+
+    @property
+    def stdout(self):
+        return self._stdout
+
+    @property
+    def stderr(self):
+        return self._stderr
+
+    def start(self):
+        self._proc = subprocess.Popen(args=self._args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        self._thread_stdout = threading.Thread(
+            target=_read_into_list, args=[self._proc.stdout, self._stdout], daemon=True
+        )
+        self._thread_stderr = threading.Thread(
+            target=_read_into_list, args=[self._proc.stderr, self._stderr], daemon=True
+        )
+        self._thread_stdout.start()
+        self._thread_stderr.start()
