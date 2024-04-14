9a41ecf765cc764d47d0967ff88ae4a71dec009c
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-14 08:40:29 +0000

    Add test for table without pk

--- tests/test_wal2json_scenarios.py
+++ tests/test_wal2json_scenarios.py
@@ -14,6 +14,76 @@ from tests.utils import DbActivitySimulator, DbStreamConsumer
 logger = logging.getLogger(__name__)
 
 
+def test_crud_on_table_without_pk(conn: Connection, conn2: Connection, drop_slot, table_name: str):
+    statements = [
+        ("INSERT INTO {table_name} (NAME) VALUES ('this-is-the-value-1')", []),
+        ("INSERT INTO {table_name} (NAME) VALUES ('this-is-the-value-2')", []),
+        ("INSERT INTO {table_name} (NAME) VALUES ('this-is-the-value-3')", []),
+        ("INSERT INTO {table_name} (NAME) VALUES ('this-is-the-value-4')", []),
+        ("UPDATE {table_name} SET NAME = 'this-is-the-value-4-new' WHERE NAME = 'this-is-the-value-4'", []),
+    ]
+    # https://github.com/eulerto/wal2json?tab=readme-ov-file
+    options = {"format-version": "2"}
+
+    create_table = f"""
+    CREATE TABLE {table_name} (
+        name varchar not null
+    )
+    """
+
+    db_activity_simulator = DbActivitySimulator(conn, table_name, statements, create_table_ddl=create_table)
+    db_stream_consumer = DbStreamConsumer(conn2, options=options)
+
+    db_stream_consumer.start_replication().start()
+    db_activity_simulator.start()
+    db_activity_simulator.join_or_fail(timeout=3)
+
+    db_stream_consumer.join_or_fail(timeout=3)
+
+    pprint(db_stream_consumer.payloads_parsed, indent=4, sort_dicts=True, compact=False)
+
+    expected = [
+        {"action": "B"},
+        {"action": "C"},
+        {"action": "B"},
+        {
+            "action": "I",
+            "columns": [{"name": "name", "type": "character varying", "value": "this-is-the-value-1"}],
+            "schema": "public",
+            "table": table_name.lower(),
+        },
+        {"action": "C"},
+        {"action": "B"},
+        {
+            "action": "I",
+            "columns": [{"name": "name", "type": "character varying", "value": "this-is-the-value-2"}],
+            "schema": "public",
+            "table": table_name.lower(),
+        },
+        {"action": "C"},
+        {"action": "B"},
+        {
+            "action": "I",
+            "columns": [{"name": "name", "type": "character varying", "value": "this-is-the-value-3"}],
+            "schema": "public",
+            "table": table_name.lower(),
+        },
+        {"action": "C"},
+        {"action": "B"},
+        {
+            "action": "I",
+            "columns": [{"name": "name", "type": "character varying", "value": "this-is-the-value-4"}],
+            "schema": "public",
+            "table": table_name.lower(),
+        },
+        {"action": "C"},
+        {"action": "B"},
+        {"action": "C"},
+    ]
+
+    assert db_stream_consumer.payloads_parsed == expected
+
+
 def test_crud_on_table_with_pk(conn: Connection, conn2: Connection, drop_slot, table_name: str):
     statements = [
         ("INSERT INTO {table_name} (NAME) VALUES ('this-is-the-value-1')", []),
