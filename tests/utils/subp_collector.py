ee837a0521b7e5c2fd10b71e74cdb605e0f58cf7
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-15 18:55:13 +0000

    Fix bug that make test fail

--- tests/utils/subp_collector.py
+++ tests/utils/subp_collector.py
@@ -28,7 +28,7 @@ class SubProcCollector:
         self._proc: subprocess.Popen | None = None
         self._stdout: list[str] = []
         self._thread_stdout: threading.Thread | None = None
-        self._stdout_line_last_found: int = 0
+        self._stdout_start_at: int = 0
 
     def poll(self) -> int | None:
         return self._proc.poll()
@@ -51,14 +51,16 @@ class SubProcCollector:
         timeout = float(timeout)
         start_time = time.monotonic()
         print(f"Waiting for: '{text}'")
+
         while time.monotonic() - start_time < timeout:
-            for line_num in range(0 if from_beginning else self._stdout_line_last_found, len(self._stdout)):
+            for line_num in range(0 if from_beginning else self._stdout_start_at, len(self._stdout)):
                 if not from_beginning:
-                    self._stdout_line_last_found = line_num
-                line = self._stdout[line_num]
-                if text in line:
-                    return line
+                    self._stdout_start_at = line_num + 1
+                logger.debug("wait_for('%s') - line: '%s'", text, self._stdout[line_num])
+                if text in self._stdout[line_num]:
+                    return self._stdout[line_num]
             time.sleep(0.01)
+
         raise TimeoutError(f"Timeout. Not found: {text}")
 
     @cached_property
@@ -77,16 +79,17 @@ class SubProcCollector:
         timeout = float(timeout)
         start_time = time.monotonic()
         print("Waiting for change...")
-        while time.monotonic() - start_time < timeout:
-            for line_num in range(0 if from_beginning else self._stdout_line_last_found, len(self._stdout)):
-                if not from_beginning:
-                    self._stdout_line_last_found = line_num
 
+        while time.monotonic() - start_time < timeout:
+            for line_num in range(0 if from_beginning else self._stdout_start_at, len(self._stdout)):
+                if not from_beginning:
+                    self._stdout_start_at = line_num + 1
+                logger.debug("wait_for_change('%s') - line: '%s'", self._stdout[line_num])
                 change = self._get_change(self._stdout[line_num])
                 if change is not None:
                     return change
-
             time.sleep(0.01)
+
         raise TimeoutError("Timeout. Change not found")
 
     def start(self) -> "SubProcCollector":
