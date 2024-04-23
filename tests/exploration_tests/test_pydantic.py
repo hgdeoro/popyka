bbb88ec1dd4bb84a41d5d18b0fd34bfa0867f36d
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-23 15:04:39 +0000

    Add test for 'config'

--- tests/exploration_tests/test_pydantic.py
+++ tests/exploration_tests/test_pydantic.py
@@ -25,7 +25,7 @@ class SamplePopykaStrictConfig(SamplePopykaConfig):
     model_config: ConfigDict = ConfigDict(extra="forbid")
 
 
-def test_serialize():
+def test_serialize_simple():
     config = SamplePopykaConfig(
         dsn="postgresql://...",
         filters=[SampleFilterConfig(class_fqn="some.Filter")],
@@ -35,11 +35,61 @@ def test_serialize():
     print(config.model_dump_json(indent=4))
 
 
-def test_deserialize():
+def test_serialize_complex():
+    config = SamplePopykaConfig(
+        dsn="postgresql://...",
+        filters=[
+            SampleFilterConfig(
+                class_fqn="some.Filter",
+                config={
+                    "key1": "value1",
+                    "key2": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                    "key3": {"subkey": [None]},
+                },
+            )
+        ],
+        processors=[SampleProcessorConfig(class_fqn="some.Processor")],
+    )
+    print(config.model_dump())
+    print(config.model_dump_json(indent=4))
+
+
+def test_deserialize_simple():
     deserialized = {
         "dsn": "postgresql://...",
-        "filters": [{"class_fqn": "some.Filter"}],
+        "filters": [
+            {"class_fqn": "some.Filter"},
+            {
+                "class_fqn": "some.other.Filter",
+                "config": {
+                    "this-is": "free-form-dict",
+                    "key1": 1234,
+                    "key2": [1, 2, 3],
+                    "key3": {
+                        "sub1": None,
+                    },
+                },
+            },
+        ],
         "processors": [{"class_fqn": "some.Processor"}],
+    }
+    config = SamplePopykaConfig(**deserialized)
+    print(config)
+
+
+def test_deserialize_complex():
+    deserialized = {
+        "dsn": "postgresql://...",
+        "filters": [
+            {
+                "class_fqn": "some.Filter",
+            },
+        ],
+        "processors": [
+            {
+                "class_fqn": "some.Processor",
+            },
+        ],
     }
     config = SamplePopykaConfig(**deserialized)
     print(config)
