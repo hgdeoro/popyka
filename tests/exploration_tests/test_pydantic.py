29d8d838414c56be89ee7f07f58973557367c59b
Horacio Guillermo de Oro <hgdeoro@gmail.com>
2024-04-23 14:40:52 +0000

    Add exploration test for pydantic

---
+++ tests/exploration_tests/test_pydantic.py
@@ -0,0 +1,49 @@
+import json
+
+from pydantic import BaseModel
+
+
+class SampleFilterConfig(BaseModel):
+    class_fqn: str
+
+
+class SampleProcessorConfig(BaseModel):
+    class_fqn: str
+
+
+class SamplePopykaConfig(BaseModel):
+    dsn: str
+    filters: list[SampleFilterConfig]
+    processors: list[SampleProcessorConfig]
+
+
+def test_serialize():
+    config = SamplePopykaConfig(
+        dsn="postgresql://...",
+        filters=[SampleFilterConfig(class_fqn="some.Filter")],
+        processors=[SampleProcessorConfig(class_fqn="some.Processor")],
+    )
+    print(config.model_dump())
+    print(config.model_dump_json(indent=4))
+
+
+def test_deserialize():
+    deserialized = json.loads(
+        """
+    {
+        "dsn": "postgresql://...",
+        "filters": [
+            {
+                "class_fqn": "some.Filter"
+            }
+        ],
+        "processors": [
+            {
+                "class_fqn": "some.Processor"
+            }
+        ]
+    }
+    """
+    )
+    config = SamplePopykaConfig(**deserialized)
+    print(config)
